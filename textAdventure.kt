77d182ad784123d80d4a1b8ffe2f09779d2ab479
damconover <123193877+damconover@users.noreply.github.com>
2023-04-15 01:48:55 -0500

    Update textAdventure.kt

--- textAdventure.kt
+++ textAdventure.kt
@@ -1,10 +1,18 @@
+//Damien Conover
+
+//Current 04/15/2023
+
+//Due 04/16/2023
+
+
+
 fun main() {
     var currentLocation = "dusty castle room"
 
     while (true) {
         when (currentLocation) {
             "dusty castle room" -> {
-                println("You are in a dusty castle room. Passages lead to the north and south. What direction?")
+                println("You are in a dusty castle room. Passages lead to the north and south. Which direction do you want to go?")
                 val direction = readln()
                 if (direction == "n") {
                     currentLocation = "armory"
@@ -13,14 +21,14 @@ fun main() {
                 }
             }
             "armory" -> {
-                println("You are in the armory. There is a room off to the south. What direction?")
+                println("You are in an armory. There is a room off to the south. Which direction do you want to go?")
                 val direction = readln()
                 if (direction == "s") {
                     currentLocation = "dusty castle room"
                 }
             }
             "torch-lit hallway" -> {
-                println("You are in a torch-lit hallway. There are rooms to the east and west. What direction?")
+                println("You are in a torch-lit hallway. There are rooms to the east and west. Which direction do you want to go?")
                 val direction = readln()
                 if (direction == "e") {
                     currentLocation = "bedroom"
@@ -29,14 +37,14 @@ fun main() {
                 }
             }
             "bedroom" -> {
-                println("You are in a bedroom. A window overlooks the castle courtyard. A hallway is to the west. What direction?")
+                println("You are in a bedroom. A window overlooks the castle courtyard. There is a hallway to the west. Which direction do you want to go?")
                 val direction = readln()
                 if (direction == "w") {
                     currentLocation = "torch-lit hallway"
                 }
             }
             "kitchen" -> {
-                println("You are in the kitchen. It looks like a roast is being made for supper. A hallway is to the east. What direction?")
+                println("You are in a kitchen. It looks like a roast is being made for supper. There is a hallway to the east. Which direction do you want to go?")
                 val direction = readln()
                 if (direction == "w") {
                     println("Can't go that way.")
